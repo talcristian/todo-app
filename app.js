36303d680ed894e0b76a8883dbd598c783603991
talcristian <31998159+talcristian@users.noreply.github.com>
2022-02-18 10:56:40 -0500

    todo list able to add items

--- app.js
+++ app.js
@@ -3,29 +3,38 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = [];
+
 app.set('view engine', 'ejs');
 
-app.use(express.static("public"));
+app.use(express.static("views"));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", (req, res) => {
+app.get("/",(req, res) => {
   var today = new Date();
-  var currentDay = today.getDay();
-  var dayStr = new Intl.DateTimeFormat('en-US',{weekday: 'long'}).format(today.getDay());
+  var options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+  }
 
-  /*if (currentDay === 6 || currentDay === 0) {
-    res.write("<h1>Yay it's the weekend</h1>");
-    res.send();
-  } else {
-    res.sendFile(__dirname + "/index.html")
-  }*/
+  var day = today.toLocaleDateString("en-US", options);
+
 
   res.render("list", {
-    day: dayStr,
-    name: "Cristian",
+    kindOfDay: day,
+    itemArray: items,
   });
-
 });
 
+app.post("/addItem", (req, res) => {
+  var itemText = req.body.newItem;
+  items.push(itemText);
+
+  console.log(itemText);
+
+  res.redirect("/");
+})
 
 
 
@@ -35,3 +44,32 @@ app.get("/", (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server started on port 3000");
 });
+
+
+
+
+
+
+
+
+
+/*
+app.get("/", (req, res) => {
+  var today = new Date();
+  var currentDay = today.getDay();
+  var dayStr = new Intl.DateTimeFormat('en-US',{weekday: 'long'}).format(today.getDay());
+
+  if (currentDay === 6 || currentDay === 0) {
+    res.write("<h1>Yay it's the weekend</h1>");
+    res.send();
+  } else {
+    res.sendFile(__dirname + "/index.html")
+  }
+
+  res.render("list", {
+    day: dayStr,
+    name: "Cristian",
+  });
+
+});
+*/
